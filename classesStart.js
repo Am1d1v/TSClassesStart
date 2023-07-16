b7d68535f122914eaae0853432f938bf57a4c413
Am1d1v <124504237+Am1d1v@users.noreply.github.com>
2023-07-16 19:29:35 +0300

    Add files via upload

---
+++ classesStart.js
@@ -0,0 +1,8 @@
+"use strict";
+class User {
+    constructor(name) {
+        this.name = name;
+    }
+}
+const user = new User('Dima');
+console.log(user);
---
+++ classesStart.ts
@@ -0,0 +1,12 @@
+
+
+class User {
+    name: string;
+
+    constructor(name: string){
+        this.name = name;
+    }
+}
+
+const user = new User('Dima');
+console.log(user);
