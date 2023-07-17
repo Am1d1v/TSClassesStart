0c8028332e1d722fe9e8ac9fe52920ac127d40e7
Am1d1v <124504237+Am1d1v@users.noreply.github.com>
2023-07-17 14:38:53 +0300

    Add files via upload
    Overloaad

--- classesStart.js
+++ classesStart.js
@@ -1,11 +1,19 @@
 "use strict";
 class User {
-    constructor(name) {
-        if (typeof name === 'string') {
-            this.name = name;
+    constructor(ageOrName, age) {
+        if (typeof ageOrName === 'string') {
+            this.name = ageOrName;
+        }
+        else if (typeof ageOrName === 'number') {
+            this.age = ageOrName;
         }
         ;
+        if (typeof age === 'number') {
+            this.age = age;
+        }
     }
 }
 const user = new User('Dima');
 const user1 = new User();
+const user2 = new User(30);
+const user3 = new User('Dima', 20);
--- classesStart.ts
+++ classesStart.ts
@@ -7,15 +7,21 @@ class User {
     constructor();
     constructor(name: string);
     constructor(age: number);
-    constructor(ageOrName?: string | number){
+    constructor(name: string, age: number);
+    constructor(ageOrName?: string | number, age?: number){
         if(typeof ageOrName ==='string'){
             this.name = ageOrName
         } else if (typeof ageOrName === 'number'){
             this.age = ageOrName
         };
+
+        if(typeof age ==='number'){
+            this.age = age
     }
+ }
 }
 
 const user = new User('Dima');
 const user1 = new User();
 const user2 = new User(30);
+const user3 = new User('Dima', 20);
