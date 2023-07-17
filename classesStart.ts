491f0a2032961e9984ab1d310213e8b8620cbcff
Am1d1v <124504237+Am1d1v@users.noreply.github.com>
2023-07-17 14:26:16 +0300

    Add files via upload

--- classesStart.ts
+++ classesStart.ts
@@ -2,15 +2,20 @@
 
 class User {
     name: string;
+    age: number;
 
     constructor();
     constructor(name: string);
-    constructor(name?: string){
-        if(typeof name ==='string'){
-            this.name = name
+    constructor(age: number);
+    constructor(ageOrName?: string | number){
+        if(typeof ageOrName ==='string'){
+            this.name = ageOrName
+        } else if (typeof ageOrName === 'number'){
+            this.age = ageOrName
         };
     }
 }
 
 const user = new User('Dima');
 const user1 = new User();
+const user2 = new User(30);
