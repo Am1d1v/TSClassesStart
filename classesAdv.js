ee95588809b075ffcc5dc8b6f2d0c62b169ac2ce
Am1d1v <124504237+Am1d1v@users.noreply.github.com>
2023-07-19 16:34:31 +0300

    Add files via upload

--- classesAdv.js
+++ classesAdv.js
@@ -1,4 +1,13 @@
 "use strict";
 class Vehicle {
+    set model(m) {
+        this._model = m;
+    }
+    get moder() {
+        return this._model;
+    }
+    addDamage(damage) {
+        this.damages.push(damage);
+    }
 }
 new Vehicle().make = 'd';
--- classesAdv.ts
+++ classesAdv.ts
@@ -2,6 +2,21 @@
 
 class Vehicle {
     make: string;
+    private damages: string[];
+    private _model: string;
+
+    set model(m: string){
+        this._model = m;
+    }
+
+    get moder(){
+        return this._model;
+    }
+
+    addDamage(damage: string){
+        this.damages.push(damage);
+    }
 }
 
 new Vehicle().make = 'd';
+
